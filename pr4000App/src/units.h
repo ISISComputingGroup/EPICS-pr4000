6ad0c2873d0aea7c17552ba4f12248ebe1f26667
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2019-08-18 17:36:06 +0100

    Fix crash due to memory corruption, add aSub argument checking
    * fix pointer assignment and usage with strings (caused memory corruption)
    * fix use of long* rather than epicsInt32* (could cause issues on linux)
    * add type checking of aSub arguments
    * use case sensitive comparison (mBar and MBar both valid and not the same)
    * make functions extern to allow testing in separate module

---
+++ pr4000App/src/units.h
@@ -0,0 +1,16 @@
+#ifndef UNITS_H
+#define UNITS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+extern long units_number_to_string(aSubRecord *prec);
+
+extern long units_string_to_number(aSubRecord *prec);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* UNITS_H */
